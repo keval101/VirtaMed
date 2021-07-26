2b6ee908fd0e64103ad9a6a938e952cca19b271c
keval101 <kevalvadhiya101@gmail.com>
2021-07-26 22:30:48 +0530

    initialize project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+interface City{
+  name:string,
+  code:string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'VirtaMed';
+
+  cities: City[];
+  selectedCity: string;
+
+  constructor() {
+      this.cities = [
+          {name: 'New York', code: 'NY'},
+          {name: 'Rome', code: 'RM'},
+          {name: 'London', code: 'LDN'},
+          {name: 'Istanbul', code: 'IST'},
+          {name: 'Paris', code: 'PRS'}
+      ];
+  }
+
+}
+
+function input() {
+  throw new Error('Function not implemented.');
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import {DropdownModule} from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    DropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
